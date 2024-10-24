910837a559a8581df7f568e0de563a396cf34c0e
Joan_Montanya <2023_joan.montanya@iticbnc.cat>
2024-10-24 18:56:21 +0200

    Versio 0.1

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+//Declarem les variables globals
+let nomjugador = "default";
+let puntuació;
+let estatDelJoc;
+//Fem els events listeners de la primera Pagina
+document.getElementById("bcp").addEventListener("click",comencaPartida)
+
+//Fem el text de la pagina inicial
+document.getElementById("infonavegador").textContent = navigator.userAgent
+document.getElementById("infoURL").textContent = window.location.href
+
+//Fem les funcions
+function comencaPartida(){
+    nomjugador = document.getElementById("nomi").value
+    window.location.href = "joc.html"
+}
